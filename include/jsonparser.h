55a28ca975f89fcfa6077819d440a3823a97d640
Berkay Şahin <berkayshn97@gmail.com>
2021-04-29 21:02:25 +0300

    kilit dosyasi okuma ve agaca ekleme islemleri yapildi

---
+++ include/jsonparser.h
@@ -0,0 +1,13 @@
+#ifndef _JSONPARSER_H_ 
+#define _JSONPARSER_H_ 
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "jrb.h"
+#include "fields.h"
+
+extern JRB   parse_json_file();
+extern char *find_key_or_val(char *p_word);
+
+#endif
---
+++ src/jsonparser.c
@@ -0,0 +1,60 @@
+#include "jsonparser.h"
+
+/*
+@description : Standart inputtan gelen dosya ismi ile kilit dosyasını açıp içerisindeki verileri 'red black tree' ağacına yerleştirecektir. 
+@author: BERKAY ŞAHİN - G191210302
+@return: tree - jsondan cikarilmis key-val ciftlerinin bulundugu agaci geri dondurur.
+*/
+JRB parse_json_file(){
+  JRB   tree;
+  IS    input_struct;
+  int   nsize;
+  int   i;
+  char *key;
+  char *val;
+
+  input_struct = new_inputstruct(NULL);
+  tree = make_jrb();
+  i = 0;
+  
+  while (get_line(input_struct) >= 0) {
+    if (input_struct->NF > 1) {
+      if(input_struct->fields[0][0] == '{')
+        continue;
+      else if(input_struct->fields[0][0] == '}')
+        break;
+
+      key = find_key_or_val(input_struct->fields[0]);
+      val = find_key_or_val(input_struct->fields[1]);
+
+      jrb_insert_str(tree, key, new_jval_s(val));
+    }
+  }
+  
+  jettison_inputstruct(input_struct);
+  return tree;
+}
+
+/*
+@description : Parametre olarak gelen ifadeyi json tipinden temizleyip geriye döndürecektir.
+@author: BERKAY ŞAHİN - G191210302
+@param: p_word - suslu parantez iceren kelime bolumudur
+@return: return_word - key/val bilgisini dondurur
+*/
+char *find_key_or_val(char *p_word){
+  char *return_word;
+  int   i;
+
+  for(i = 1; p_word[i] != '\"'; i++){
+    p_word[i - 1] = p_word[i];
+    if(p_word[i + 1] == '\"'){
+      p_word[i] = '\0';
+      break;  
+    }
+  }
+
+  return_word = (char *) malloc(sizeof(char)*(strlen(p_word) + 1));
+  strcpy(return_word, p_word);
+
+  return return_word;
+}

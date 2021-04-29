fa2173e1245990b5a65e939dd043ecbb1f9dbe3e
Berkay Şahin <berkayshn97@gmail.com>
2021-04-29 21:03:58 +0300

    ornek dosyalar kaldirildi

--- include/message.h
+++
@@ -1,6 +0,0 @@
-#ifndef MESSAGE_H
-#define MESSAGE_H
-
-void mesaj_yazdir(char ch[20]);
-
-#endif
--- src/message.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include "message.h"
-
-void mesaj_yazdir(char ch[20]){
-  printf("Girilen mesaj : %s \n", ch);
-}

1682e57713661eb4086b9cae7ada07821dadd565
Berkay Şahin <berkayshn97@gmail.com>
2021-04-29 21:03:29 +0300

    Agac yapisini gostermek icin main degistirildi

--- src/main.c
+++ src/main.c
@@ -1,7 +1,23 @@
-#include "message.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "jrb.h"
+#include "fields.h"
+#include "jsonparser.h"
 
-int main(){
-  char ch[20] = "Merhaba";
-  mesaj_yazdir(ch);
+int main()
+{
+  JRB tree;
+  JRB tmp;
+
+  tree = parse_json_file();
+
+  printf("\n------------------- Agac --------------------\n\n");
+  jrb_traverse(tmp, tree) {
+    printf("%-40s %2s\n", tmp->key.s, tmp->val.s);
+  }
+  printf("\n---------------------------------------------\n\n");
+
+  jrb_free_tree(tree);
   return 0;
 }

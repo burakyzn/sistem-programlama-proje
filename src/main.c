bebad9d503391735e4919467ed2109245c064493
Burak Yazan <brkyznn@gmail.com>
2021-04-30 19:52:16 +0300

    main decoder ornegi calisacak sekilde duzenlendi

--- src/main.c
+++ src/main.c
@@ -1,14 +1,18 @@
 #include "jrb.h"
 #include "jsonparser.h"
 #include "encoder.h"
+#include "decoder.h"
 
 int main()
 {
   JRB tree;
   JRB tmp;
 
-  tree = parse_json_file();
-  encode_file(tree);
+  // tree = parse_json_file(1); // encode icin 1 yollanmali
+  // encode_file(tree);
+
+  tree = parse_json_file(0);
+  decode_file(tree);
 
   jrb_free_tree(tree);
   return 0;

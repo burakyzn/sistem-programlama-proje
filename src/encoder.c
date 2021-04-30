06fe43922e2e6173796f1646fb6fce359059fcb3
MeftunH <maftunhashimli@gmail.com>
2021-04-30 20:07:14 +0400

    Kod duzenini iyilestirmek icin trim islemi ayrildi

--- src/encoder.c
+++ src/encoder.c
@@ -1,25 +1,5 @@
 #include "encoder.h"
-
-char *ltrim(char *p_word)
-{
-  while(isspace(*p_word)) 
-    p_word++;
-    
-  return p_word;
-}
-
-char *rtrim(char *p_word)
-{
-  char* back = p_word + strlen(p_word);
-  while(isspace(*--back));
-  *(back+1) = '\0';
-  return p_word;
-}
-
-char *trim(char *p_word)
-{
-  return rtrim(ltrim(p_word)); 
-}
+#include "trim.h"
 
 /*
 @description : Parametre olarak kelime-sifre eslerini tasiyan agaci alir ve bu agaci
